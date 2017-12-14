abb3bf8c57d0734739a3a50ff012331d87b1440d
Jon Caviness <Jon@Jons-Mac.local>
2017-12-13 21:57:21 -0700

    updated

--- src/components/Images.js
+++ src/components/Images.js
@@ -5,22 +5,22 @@ var pub = process.env.PUBLIC_URL;
 function makeArray() {
    for(let i=0; i < 13; i++) {
       imageArray.push({
-         image: `${pub}/cardDeck/${i + 2}c.png`,
+         image: `cardDeck/${i + 2}c.png`,
          id: ((i + 1) * 4) - 3,
          guessed: false
       });
       imageArray.push({
-         image: `${pub}/cardDeck/${i + 2}d.png`,
+         image: `cardDeck/${i + 2}d.png`,
          id: ((i + 1) * 4) - 2,
          guessed: false
       });
       imageArray.push({
-         image: `${pub}/cardDeck/${i + 2}h.png`,
+         image: `cardDeck/${i + 2}h.png`,
          id: ((i + 1) * 4) - 1,
          guessed: false
       });
       imageArray.push({
-         image: `${pub}/cardDeck/${i + 2}s.png`,
+         image: `cardDeck/${i + 2}s.png`,
          id: ((i + 1) * 4),
          guessed: false
       });
